419f766acbe5ec83e7dd627ee975ce4c62919426
ZeinTonconi <zein.tonconi@gmail.com>
2024-07-09 17:41:16 -0400

    Me Encargo route

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+app.get('/meencargo', (req: Request, res: Response) => {
+  res.send("Bienvenido a Me Encargo")
+})
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
