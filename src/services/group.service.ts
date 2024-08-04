6b5d2e8a2c8a82ada8284c5f7757e523460d49a3
AndresSanchez <sanchezandres8203@gmail.com>
2024-08-04 17:50:12 -0400

    feat: update group

--- src/services/group.service.ts
+++ src/services/group.service.ts
@@ -1,3 +1,4 @@
+import { IGroup } from "../entities/IGroup"
 import { GroupRepository } from "../repositories/group.repository"
 import { ProductRepository } from "../repositories/product.repository"
 
@@ -5,7 +6,7 @@ import { ProductRepository } from "../repositories/product.repository"
 
 const getProductsByGroup = async (idGroup: number) => {
     const group = await GroupRepository.getGroupById(idGroup)
-    if(!group)
+    if (!group)
         throw new Error("Group with such id doesn't exist")
     return await GroupRepository.getProductsInGroup(group)
 }
@@ -13,7 +14,7 @@ const getProductsByGroup = async (idGroup: number) => {
 const getAllGroups = async () => {
     const groups = await GroupRepository.getAllGroupsWithProducts();
     const resGroups = groups.map((group) => {
-        return {...group, products: group.products.slice(0,1)}
+        return { ...group, products: group.products.slice(0, 1) }
     })
     return resGroups
 }
@@ -22,8 +23,16 @@ const getAllGroupsWithProducts = async () => {
     return await GroupRepository.getAllGroupsWithProducts()
 }
 
+const updateGroup = async (newData: any, groupId: number) => {
+    const group = await GroupRepository.getGroupById(groupId)
+    if (!group) throw new Error(`Group with id ${groupId} doesn't exist`)
+    return await GroupRepository.updateGroup(newData, group)
+}
+
+
 export const GroupService = {
     getProductsByGroup,
     getAllGroups,
-    getAllGroupsWithProducts
+    getAllGroupsWithProducts,
+    updateGroup
 }
