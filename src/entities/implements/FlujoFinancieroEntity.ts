9b3d7ced5e720bf76770d85dfcbb8e22a7e90466
Jhonatan <sellooricalcos@gmail.com>
2024-08-03 20:47:35 -0400

    Changed flujoFinancieroEntity

--- src/entities/implements/FlujoFinancieroEntity.ts
+++ src/entities/implements/FlujoFinancieroEntity.ts
@@ -25,7 +25,7 @@ export class FlujoFinancieroEntity implements IFlujoFinanciero {
     @Column({ nullable: false, default: () => 'CURRENT_TIMESTAMP(6)' })
     fecha!: Date;
 
-    @Column({nullable: false})
+    @Column({nullable: true})
     id_vendedor!: number;
 
     @Column({nullable: false})
