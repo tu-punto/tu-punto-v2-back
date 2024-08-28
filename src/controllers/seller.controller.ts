22237c12f4a1e546454d24c24de205f54e5dd8d8
AndresSanchez <sanchezandres8203@gmail.com>
2024-08-27 20:12:42 -0400

    feat: add get shippings by seller id endpoint

--- src/controllers/seller.controller.ts
+++ src/controllers/seller.controller.ts
@@ -2,30 +2,30 @@ import { Request, Response } from "express";
 import { SellerService } from "../services/seller.service";
 
 export const getSellers = async (req: Request, res: Response) => {
-    const sellers = await SellerService.getAllSellers();
-    res.json(sellers)
-}
+  const sellers = await SellerService.getAllSellers();
+  res.json(sellers);
+};
 
 export const registerSeller = async (req: Request, res: Response) => {
-    const seller = req.body;
-    try {
-        const newSeller = await SellerService.registerSeller(seller)
-        res.json({
-            status: true,
-            newSeller
-        })
-    } catch (error) {
-        res.status(500).json({ msg: 'Internal server error', error })
-    }
-}
+  const seller = req.body;
+  try {
+    const newSeller = await SellerService.registerSeller(seller);
+    res.json({
+      status: true,
+      newSeller,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: "Internal server error", error });
+  }
+};
 
 export const updateSeller = async (req: Request, res: Response) => {
-    const sellerId = parseInt(req.params.id)
-    const { newData } = req.body
-    try {
-        const updatedSeller = await SellerService.updateSeller(sellerId, newData)
-        res.json({ status: true, updatedSeller })
-    } catch (error) {
-        res.status(500).json({ msg: 'Internal server error', error })
-    }
-}
+  const sellerId = parseInt(req.params.id);
+  const { newData } = req.body;
+  try {
+    const updatedSeller = await SellerService.updateSeller(sellerId, newData);
+    res.json({ status: true, updatedSeller });
+  } catch (error) {
+    res.status(500).json({ msg: "Internal server error", error });
+  }
+};
--- src/controllers/shipping.controller.ts
+++ src/controllers/shipping.controller.ts
@@ -2,69 +2,87 @@ import { Request, Response } from "express";
 import { ShippingService } from "../services/shipping.service";
 
 export const getShipping = async (req: Request, res: Response) => {
-    try {
-        const shippings = await ShippingService.getAllShippings();
-        res.json(shippings);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
+  try {
+    const shippings = await ShippingService.getAllShippings();
+    res.json(shippings);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
-export const getShippingByIds = async(req:Request, res:Response) =>{
-    const { ids } = req.params;
-    try{
-        const idsArray = ids.split(',').map(id => parseInt(id.trim()));
-        const shippings = await ShippingService.getShippingByIds(idsArray);
-        res.json(shippings);
-    }catch(error){
-        console.log(error);
-        res.status(500).json({error: 'Error getting shippings by Ids'})
-    }
-}
+export const getShippingByIds = async (req: Request, res: Response) => {
+  const { ids } = req.params;
+  try {
+    const idsArray = ids.split(",").map((id) => parseInt(id.trim()));
+    const shippings = await ShippingService.getShippingByIds(idsArray);
+    res.json(shippings);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error getting shippings by Ids" });
+  }
+};
 export const registerShipping = async (req: Request, res: Response) => {
-    const shipping = req.body;
-    try {
-        const newShipping = await ShippingService.registerShipping(shipping)
-        res.json({
-            status: true,
-            newShipping
-        })
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
+  const shipping = req.body;
+  try {
+    const newShipping = await ShippingService.registerShipping(shipping);
+    res.json({
+      status: true,
+      newShipping,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 export const registerSaleToShipping = async (req: Request, res: Response) => {
-    const { shippingId, sales } = req.body
-    try {
-        const savedSales = []
+  const { shippingId, sales } = req.body;
+  try {
+    const savedSales = [];
 
-        for (let sale of sales) {
-            const saleShipping = await ShippingService.registerSaleToShipping(shippingId, sale)
-            savedSales.push(saleShipping)
-
-        }
-        res.json(savedSales)
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ msg: 'Shipping Internal Server Error', error })
+    for (let sale of sales) {
+      const saleShipping = await ShippingService.registerSaleToShipping(
+        shippingId,
+        sale
+      );
+      savedSales.push(saleShipping);
     }
-}
+    res.json(savedSales);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Shipping Internal Server Error", error });
+  }
+};
 
 const updateShipping = async (req: Request, res: Response) => {
-    const id = parseInt(req.params.id)
-    const { newData } = req.body
-    try {
-        const shippingUpdated = await ShippingService.updateShipping(newData, id)
-        res.json(shippingUpdated)
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ msg: 'Internal Server Error', error })
-    }
-}
+  const id = parseInt(req.params.id);
+  const { newData } = req.body;
+  try {
+    const shippingUpdated = await ShippingService.updateShipping(newData, id);
+    res.json(shippingUpdated);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Internal Server Error", error });
+  }
+};
+
+export const getShippingsBySellerController = async (
+  req: Request,
+  res: Response
+) => {
+  const id = parseInt(req.params.id);
+  try {
+    const shippingsBySeller = await ShippingService.getShippingsBySellerService(
+      id
+    );
+    res.json(shippingsBySeller);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Internal Server Error", error });
+  }
+};
 
 export const ShippingController = {
-    updateShipping
-}
+  updateShipping,
+};
