e7e13359cff83eabe5d546ed67a4cc493d337157
AndresSanchez <sanchezandres8203@gmail.com>
2024-12-11 20:16:46 -0400

    fixed daily effective

--- src/entities/IEfectivoDiario.ts
+++ src/entities/IEfectivoDiario.ts
@@ -2,10 +2,19 @@ import { CierreCajaEntity } from "./implements/CierreCajaEntity";
 
 export interface IEfectivoDiario {
   id_efectivo_diario: number;
-  valor: number;
-  cantidad: number;
-  total: number;
+  corte_0_2: number;
+  corte_0_5: number;
+  corte_1: number;
+  corte_2: number;
+  corte_5: number;
+  corte_10: number;
+  corte_20: number;
+  corte_50: number;
+  corte_100: number;
+  corte_200: number;
+  total_coins: number;
+  total_bills: number;
   created_at: Date;
   updated_at: Date;
-  id_cierre_caja: CierreCajaEntity; 
+  id_cierre_caja: CierreCajaEntity;
 }
