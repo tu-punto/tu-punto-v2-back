d90923b844a4b6a98fa4818885086f927a6f0fa2
Andrezu <sanchezandres8203@gmail.com>
2025-07-16 11:35:12 -0400

    fix: filter on sales not en espera

--- src/repositories/seller.repository.ts
+++ src/repositories/seller.repository.ts
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
-import { Types } from 'mongoose';
+import { Types } from "mongoose";
 import { VendedorSchema } from "../entities/implements/VendedorSchema";
 import { IVendedor } from "../entities/IVendedor";
 import { IVendedorDocument } from "../entities/documents/IVendedorDocument";
-import { FlujoFinancieroModel } from '../entities/implements/FlujoFinancieroSchema';
+import { FlujoFinancieroModel } from "../entities/implements/FlujoFinancieroSchema";
 import { VentaModel } from "../entities/implements/VentaSchema";
+import { PedidoModel } from "../entities/implements/PedidoSchema";
 
-const VendedorModel = mongoose.model<IVendedorDocument>("Vendedor", VendedorSchema);
+const VendedorModel = mongoose.model<IVendedorDocument>(
+  "Vendedor",
+  VendedorSchema
+);
 
 const findAll = async (): Promise<IVendedor[]> => {
   return await VendedorModel.find().lean<IVendedor[]>().exec();
@@ -21,30 +25,74 @@ const registerSeller = async (seller: IVendedor) => {
   return await newSeller.save();
 };
 
-const updateSeller = async (
-  sellerId: any,
-  updateData: Partial<IVendedor>
-) => {
-  return await VendedorModel.findByIdAndUpdate(sellerId, updateData, { new: true });
+const updateSeller = async (sellerId: any, updateData: Partial<IVendedor>) => {
+  return await VendedorModel.findByIdAndUpdate(sellerId, updateData, {
+    new: true,
+  });
 };
 
 export const incrementDebt = async (id: string, delta: number) => {
-  return await VendedorModel.findByIdAndUpdate(id, { $inc: { deuda: delta } }, { new: true })
+  return await VendedorModel.findByIdAndUpdate(
+    id,
+    { $inc: { deuda: delta } },
+    { new: true }
+  );
 };
 
 const findDebtsBySeller = async (sellerId: string) => {
   return await FlujoFinancieroModel.find({
     id_vendedor: new Types.ObjectId(sellerId),
-    esDeuda: true
+    esDeuda: true,
   })
-    .select('monto concepto fecha')
+    .select("monto concepto fecha")
     .lean()
     .exec();
 };
 
 const markSalesAsDeposited = async (sellerId: string): Promise<void> => {
+  const validSales = await VentaModel.aggregate([
+    {
+      $match: {
+        deposito_realizado: false,
+        vendedor: new Types.ObjectId(sellerId),
+      },
+    },
+    {
+      $lookup: {
+        from: "Pedido",
+        localField: "pedido",
+        foreignField: "_id",
+        as: "pedidoInfo",
+        pipeline: [
+          {
+            $project: {
+              estado_pedido: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $match: {
+        "pedidoInfo.estado_pedido": { $ne: "En Espera" },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+      },
+    },
+  ]);
+  if (validSales.length === 0) {
+    console.log("No valid sales found for the seller.");
+    return;
+  }
   await VentaModel.updateMany(
-    { vendedor: sellerId, deposito_realizado: false },
+    {
+      _id: { $in: validSales },
+      vendedor: sellerId,
+      deposito_realizado: false,
+    },
     { $set: { deposito_realizado: true } }
   );
 };
@@ -56,7 +104,5 @@ export const SellerRepository = {
   findById,
   incrementDebt,
   findDebtsBySeller,
-  markSalesAsDeposited
+  markSalesAsDeposited,
 };
-
-
