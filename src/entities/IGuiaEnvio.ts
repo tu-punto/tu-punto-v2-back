7e598950725c5c66e14641c4c5a09e7cde793857
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-08-08 02:21:01 -0400

    feat: add shipping guide schema

---
+++ src/entities/IGuiaEnvio.ts
@@ -0,0 +1,11 @@
+import { Types } from 'mongoose';
+
+export interface IGuiaEnvio {
+    _id?: Types.ObjectId;
+
+    vendedor: Types.ObjectId;
+    descripcion?: string;
+    fecha_subida: Date;
+    imagen?: Buffer;
+    tipoArchivo: "image/jpeg"|"image/png"|"image/webp";
+}
---
+++ src/entities/implements/GuiaEnvioSchema.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from 'mongoose';
+import { IGuiaEnvioDocument } from '../documents/IGuiaEnvioDocument';
+
+const GuiaEnvioSchema = new Schema({
+    vendedor: {
+        type: Schema.Types.ObjectId,
+        ref: 'Vendedor',
+        required: true
+    },
+    descripcion: {
+        type: String,
+        maxlength: 200,
+        default: ''
+    },
+    fecha_subida: {
+        type: Date,
+        default: Date.now
+    },
+    imagen: {
+        type: Buffer,
+        required: false
+    },
+    tipoArchivo: {
+        type: String,
+        required: false,
+        enum: ['image/jpeg', 'image/png', 'image/webp']
+    }
+},{
+    collection: 'GuiaEnvio',
+    timestamps: false
+})
+
+export const GuiaEnviosModel = model<IGuiaEnvioDocument>('GuiaEnvio', GuiaEnvioSchema)
